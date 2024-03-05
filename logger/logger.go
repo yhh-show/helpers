73f6eec4b96e927538fce4007f950b472cd1a532
汪光启 <echo.wang@batatagames.com>
2024-03-05 13:50:26 +0800

    更新 logger

--- logger/logger.go
+++ logger/logger.go
@@ -6,48 +6,9 @@ import (
 )
 
 var (
-	logger = log.New(os.Stderr, "", log.LstdFlags|log.Lshortfile)
+	L = log.New(os.Stderr, "", log.LstdFlags|log.Lshortfile)
 )
 
 func SetLogger(l *log.Logger) {
-	if l == nil {
-		return
-	}
-	logger = l
-}
-
-func Print(v ...any) {
-	logger.Print(v...)
-}
-
-func Printf(format string, v ...any) {
-	logger.Printf(format, v...)
-}
-
-func Println(v ...any) {
-	logger.Println(v...)
-}
-
-func Fatal(v ...any) {
-	logger.Fatal(v...)
-}
-
-func Fatalf(format string, v ...any) {
-	logger.Fatalf(format, v...)
-}
-
-func Fatalln(v ...any) {
-	logger.Fatalln(v...)
-}
-
-func Panic(v ...any) {
-	logger.Panic(v...)
-}
-
-func Panicf(format string, v ...any) {
-	logger.Panicf(format, v...)
-}
-
-func Panicln(v ...any) {
-	logger.Panicln(v...)
+	L = l
 }
