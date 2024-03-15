0ef22be23e8db027c578a277e24525d0c8f0b056
汪光启 <echo.wang@batatagames.com>
2024-03-15 17:11:26 +0800

    safego 遇到 panic 的时候报告错误

--- safego/safego.go
+++ safego/safego.go
@@ -1,12 +1,25 @@
 package safego
 
+import (
+	"fmt"
+	"github.com/yhh-show/helpers/errs"
+)
+
 // Go 防止panic的goroutine
 func Go(fn func()) chan any {
-	ch := make(chan any)
+	ch := make(chan any, 1)
 
 	go func() {
 		defer func() {
-			ch <- recover()
+			r := recover()
+			ch <- r
+			if r != nil {
+				err, ok := r.(error)
+				if !ok {
+					err = fmt.Errorf("%v", r)
+				}
+				errs.Report(err, "safego.Go panic")
+			}
 		}()
 
 		fn()
